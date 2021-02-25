eb35d739ee7db9ebd894fb6575078fc8b292e98f
Gois <matheusgoislimasilva@gmail.com>
2021-02-25 16:37:35 -0300

    update

--- fastlane_core/lib/fastlane_core/helper.rb
+++ fastlane_core/lib/fastlane_core/helper.rb
@@ -207,12 +207,11 @@ module FastlaneCore
       return FastlaneCore::Env.truthy?("FASTLANE_ITUNES_TRANSPORTER_PATH")
     end
 
-    def self.path_name_itmsp
-      if !path_name_itmsp
-        path_name_itmsp = ENV["FASTLANE_ITUNES_TRANSPORTER_PATH_ITMSP"] || "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx".gsub("x") do 
-          rand(16).to_s(16) 
-        end
+    def self.path_name_itms
+      if !@path_name_itms
+        @path_name_itms = ENV["FASTLANE_ITUNES_TRANSPORTER_PATH_ITMSP"] || "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx".gsub("x") do rand(16).to_s(16) end
       end
+      return @path_name_itms
     end
 
     def self.user_defined_itms_path
